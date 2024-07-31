289557afedf9899cff9b164706c996f4c21fb815
Yannis Hildebrand <117194816+DerYannis1@users.noreply.github.com>
2024-07-31 14:16:10 +0200

    fixed tests for external_groups

--- t/tests.py
+++ t/tests.py
@@ -10,116 +10,114 @@ cred = {
 
 api = sepsesam.api.Api(**cred, verify=False)
 
-class TestSepSesam(unittest.TestCase):
-    """ sepsesam.api unittests """
 
+class TestSepSesam(unittest.TestCase):
+    """sepsesam.api unittests"""
 
     """Managing Backup Tasks"""
 
-    
-
-    #backup_task_create
+    # backup_task_create
     def test_backupTaskCreate(self):
+        # backup_task_create
+        api.backup_task_create(
+            "Unittest_BackupTask",
+            **{"client": {"name": "localhost"}, "source": "C:/tmp"},
+        )
 
-        #backup_task_create
-        api.backup_task_create("Unittest_BackupTask", **{"client": {"name": "localhost"}, "source": "C:/tmp"})
-       
-
-        #Backup-task-get
-        self.assertEqual(api.backup_task_get("Unittest_BackupTask")["name"], "Unittest_BackupTask")
+        # Backup-task-get
+        self.assertEqual(
+            api.backup_task_get("Unittest_BackupTask")["name"], "Unittest_BackupTask"
+        )
         self.assertEqual(api.backup_task_get("Unittest_BackupTask")["client"]["id"], 0)
-        
 
-        #backup_task_delete
+        # backup_task_delete
         api.backup_task_delete("Unittest_BackupTask")
-        with self.assertRaises(sepsesam.api.SEPSesamAPIError) as context:
-           
+        with self.assertRaises(sepsesam.api.SEPSesamAPIError):
             api.backup_task_get("Unittest_BackupTask")
-            self.assertEqual(api.backup_task_get("Unittest_BackupTask")["error"], "object.not.found.id")
-        
-         
+            self.assertEqual(
+                api.backup_task_get("Unittest_BackupTask")["error"],
+                "object.not.found.id",
+            )
 
     """Managing Clients"""
 
     def test_Clients(self):
-        
-        #client create
+        # client create
         self.assertEqual(api.client_create("unittest_Client")["id"], 1)
 
-
-        #client_find
+        # client_find
         self.assertGreater(len(api.client_find(name="*")), 1)
-        self.assertEqual(api.client_find(**{"name": "unittest_Client"})[0]["name"], "unittest_Client")
+        self.assertEqual(
+            api.client_find(**{"name": "unittest_Client"})[0]["name"], "unittest_Client"
+        )
 
-
-        #client update
+        # client update
         self.assertRaises(Exception, api.client_update, None, None)
-        self.assertEqual(api.client_update(0, "localhost",**{"usercomment": "test"})["usercomment"], "test")
+        self.assertEqual(
+            api.client_update(0, "localhost", **{"usercomment": "test"})["usercomment"],
+            "test",
+        )
 
-
-        #client delete
+        # client delete
         self.assertEqual(api.client_delete("1"), 1)
         self.assertLess(len(api.client_find(name="*")), 2)
-    
 
-
-
-
-    
     """Manage external groups"""
 
     def test_externalGroups(self):
-        #external_group_create
-        self.assertEqual(api.external_group_create("Unittest_ExternalGroup",True)["externalId"], "Unittest_ExternalGroup")
+        # external_group_create
+        self.assertEqual(
+            api.external_group_create("Unittest_ExternalGroup", True)["externalId"],
+            "Unittest_ExternalGroup",
+        )
 
+        # external_group_find
+        self.assertEqual(
+            api.external_group_find(
+                **{"externalId": "Unittest_ExternalGroup", "id": 1}
+            )[0]["externalId"],
+            "Unittest_ExternalGroup",
+        )
 
-        #external_group_find
-        self.assertEqual(api.external_group_find(**{"externalId": "Unittest_ExternalGroup", "id": 1})[0]["externalId"], "Unittest_ExternalGroup")
-
-
-        #external_group_delete
+        # external_group_delete
         api.external_group_delete(1)
-        
 
-    
     """Manage Groups"""
-    
+
     def test_GroupCreateAndDelete(self):
+        # group_create
+        self.assertEqual(
+            api.group_create(
+                5,
+                **{"name": "UnittestGroup", "enabled": True, "rolesList": ["ReadOnly"]},
+            )["id"],
+            5,
+        )
 
-        #group_create
-        self.assertEqual(api.group_create(5,**{"name": "UnittestGroup","enabled": True, "rolesList": ['ReadOnly']})["id"],5)
-
-
-        #group_delete
-        self.assertEqual(api.group_delete(5),5)
-               
-
+        # group_delete
+        self.assertEqual(api.group_delete(5), 5)
 
     """Manage locations"""
 
-    #location_create and location_delete
+    # location_create and location_delete
     def test_LocationsCreateAndDelete(self):
-        
-        #location create
+        # location create
         self.assertEqual(api.location_create("UNITTEST_LOCATION")["id"], 1)
 
-
-        #location_resolve_to_id
+        # location_resolve_to_id
         self.assertEqual(api.location_resolve_to_id("UNITTEST_LOCATION"), 1)
-    
 
-        #id has to be unique    
-        with self.assertRaises(sepsesam.api.SEPSesamAPIError) as context:
-            self.assertEqual(api.location_create("UNITTEST_LOCATION2", **{"id": 1})["error"], "duplicate.entry" )
+        # id has to be unique
+        with self.assertRaises(sepsesam.api.SEPSesamAPIError):
+            self.assertEqual(
+                api.location_create("UNITTEST_LOCATION2", **{"id": 1})["error"],
+                "duplicate.entry",
+            )
 
-
-        #location delete
+        # location delete
         api.location_delete(1)
-        self.assertEqual(api.location_get(1),None)
-
-
+        self.assertEqual(api.location_get(1), None)
 
 
 if __name__ == "__main__":
     unittest.main()
-
