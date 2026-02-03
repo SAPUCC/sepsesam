571b5934ab8c274ede57abedf3ccd4469713eafb
Nurax67 <138774644+Nurax67@users.noreply.github.com>
2026-02-03 10:42:15 +0100

    Updates testsuite

--- t/tests.py
+++ t/tests.py
@@ -1,40 +1,40 @@
 import unittest
 
 # testing backup events
-from .test_backup_events import TestBackupEvents
+from t.test_backup_events import TestBackupEvents
 
 # testing backup tasks
-from .test_backup_tasks import TestBackupTasks
+from t.test_backup_tasks import TestBackupTasks
 
 # testing clients
-from .test_clients import TestClients
+from t.test_clients import TestClients
 
 # testing command events
-from .test_command_events import TestCommandEvents
+from t.test_command_events import TestCommandEvents
 
 # testing commands
-from .test_commands import TestCommands
+from t.test_commands import TestCommands
 
 # testing drive groups
-from .test_drive_groups import TestDriveGroups
+from t.test_drive_groups import TestDriveGroups
 
 # testing external groups
-from .test_external_groups import TestExternalGroups
+from t.test_external_groups import TestExternalGroups
 
 # testing groups
-from .test_groups import TestGroups
+from t.test_groups import TestGroups
 
 # testing locations
-from .test_locations import TestLocations
+from t.test_locations import TestLocations
 
 # testing media pools
-from .test_media_pools import TestMediaPools
+from t.test_media_pools import TestMediaPools
 
 # testing media
-from .test_media import TestMedia
+from t.test_media import TestMedia
 
 # testing schedules
-from .test_schedule import TestSchedules
+from t.test_schedule import TestSchedules
 
 if __name__ == "__main__":
     unittest.main()
